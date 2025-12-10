5004f626e104267951008b4a932afcc0b4ca80e1
Andrew Scouten <andrewscouten@gmail.com>
2025-12-10 17:16:17 -0600

    Another settup.py fix

--- src/milestone6/setup.py
+++ src/milestone6/setup.py
@@ -5,6 +5,9 @@ from setuptools import setup
 
 package_name = 'milestone6'
 
+# Get the directory where setup.py is located
+package_dir = os.path.dirname(os.path.abspath(__file__))
+
 setup(
     name=package_name,
     version='0.0.0',
@@ -21,7 +24,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'),
-         glob(os.path.join('launch', '*.launch.py'))),
+         glob(os.path.join(package_dir, 'launch', '*.launch.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
