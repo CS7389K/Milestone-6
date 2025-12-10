c10b13eea2f5896d58095770ad2ce10b05f5d031
Andrew Scouten <andrewscouten@gmail.com>
2025-12-10 15:06:03 -0600

    Change whisper params

--- src/milestone6/milestone6/nlp/whisper.py
+++ src/milestone6/milestone6/nlp/whisper.py
@@ -5,12 +5,16 @@ class WhisperBackend():
 
     def __init__(
         self,
-        model_type: str = "small",
+        model_type: str = "base.en",
         device: str = "cpu",
         temperature: float = 0.0
     ):
         # Initialize Whisper model
-        self.whisper = whisper.load_model(model_type, device=device)
+        self.whisper = whisper.load_model(
+            model_type, 
+            device=device,
+            in_memory=True,
+        )
         self.temperature = temperature
 
     def __call__(
