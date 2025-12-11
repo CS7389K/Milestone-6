a1f19a43fc4bbe8991c0616ff43580fb45a53d5a
Andrew Scouten <andrewscouten@gmail.com>
2025-12-11 11:39:51 -0600

    Remove bad package

--- src/milestone6/setup.py
+++ src/milestone6/setup.py
@@ -9,7 +9,6 @@ setup(
     version='0.0.0',
     packages=[
         package_name,
-        f'{package_name}.launch',
         f'{package_name}.nlp',
         f'{package_name}.nlp.backends',
         f'{package_name}.teleop',
